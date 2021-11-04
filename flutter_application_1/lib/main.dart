9ec91756ce71c378430db7f06696de6ad1f8729c
Saul <saulatylano125mail.com>
2021-11-03 23:18:19 -0600

    ejemplo2

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'App  Eufloria',
+              'App  euflori',
             ),
             Text(
               '$_counter',
